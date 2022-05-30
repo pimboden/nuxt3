792310433114d74a48e5717e83b0693987aaed77
CAMPUS\faimbode <patrick.imboden@hslu.ch>
2022-05-30 13:25:57 +0200

    fertig

---
+++ 03-DarkMode/app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { isDarkMode } = useDarkMode();
+</script>
+<template>
+  <div :style="isDarkMode ? {'background-color':'black'}:null">
+    <Nav />
+    <Heading />
+    <Cards />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Georgia;
+}
+</style>
---
+++ 03-DarkMode/composables/useDarkMode.ts
@@ -0,0 +1,12 @@
+const useDarkMode =()=>{
+
+  const isDarkMode = useState("darkMode", () => false)
+  const toggleDarkMode = () =>{
+    isDarkMode.value = !isDarkMode.value
+  }
+  return{
+    isDarkMode,
+    toggleDarkMode
+  }
+}
+export default useDarkMode;
